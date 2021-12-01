1bbdd00c2d2c4dc6178df3200daf7bf97cc2215a
Gabriella Gonzalez <Gabriel439@gmail.com>
2021-11-30 16:47:57 -0800

    Add flake

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{ inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-21.11";
+
+  outputs = { nixpkgs }: {
+    apps."x86-64_darwin".format = {
+      type = "app";
+      program = nixpkgs.haskellPackages.fourmolu;
+    };
+  };
+}
