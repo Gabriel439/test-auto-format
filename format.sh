ed8a33102add3b444ff55e4d40307e69a36c6334
Gabriella Gonzalez <Gabriel439@gmail.com>
2021-12-01 12:23:32 -0800

    Update format script

--- format.sh
+++ format.sh
@@ -1,1 +1,6 @@
-nix run github:NixOS/nixpkgs/release-21.11#legacyPackages.x86_64-darwin.haskellPackages.fourmolu -- --mode inplace $(find . -name '*.hs')
+FILES=( $(git diff --name-only --cached) )
+
+if [ "${#FILES[@]}" -ne 0 ]; then
+	nix run github:NixOS/nixpkgs/release-21.11#legacyPackages.x86_64-darwin.haskellPackages.fourmolu -- --mode inplace "${FILES[@]}"
+	git add "${FILES[@]}"
+fi
