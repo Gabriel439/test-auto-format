f8890b98f158d3203373aa16fff5e3ab6de18a53
Gabriella Gonzalez <Gabriel439@gmail.com>
2021-12-01 11:55:43 -0800

    Update format script

--- format.sh
+++ format.sh
@@ -1,1 +1,1 @@
-nix run github:NixOS/nixpkgs/release-21.11#legacyPackages.x86_64-darwin.haskellPackages.fourmolu -- --mode inplace Main.hs
+nix run github:NixOS/nixpkgs/release-21.11#legacyPackages.x86_64-darwin.haskellPackages.fourmolu -- --mode inplace $(find . -name '*.hs')
