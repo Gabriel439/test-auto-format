d9b5fe4ffdad9fdd07d054eb27e1893daee43b94
Gabriella Gonzalez <Gabriel439@gmail.com>
2021-11-30 16:53:15 -0800

    Fix system

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 { inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-21.11";
 
   outputs = { nixpkgs, ... }: {
-    apps."x86-64_darwin".format = {
+    apps."x86_64-darwin".format = {
       type = "app";
       program = nixpkgs.haskellPackages.fourmolu;
     };
