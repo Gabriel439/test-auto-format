0b8c7745e89b1b21a47b95222c12076e6232a7c5
Gabriella Gonzalez <Gabriel439@gmail.com>
2022-01-04 15:45:33 -0800

    Improve resilience of `./format` script

--- format.sh
+++ format.sh
@@ -1,4 +1,4 @@
-FILES=( $(git diff --name-only --cached) )
+IFS=$'\n' FILES=( $(git diff --name-only --cached) )
 
 if [ "${#FILES[@]}" -ne 0 ]; then
 	nix run github:NixOS/nixpkgs/release-21.11#legacyPackages.x86_64-darwin.haskellPackages.fourmolu -- --mode inplace "${FILES[@]}"
